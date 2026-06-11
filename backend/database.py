23cda15dd14fdf1852369e31c7ac6ccf925f3f6e
alanstudy2027 <alanstudy2027@gmail.com>
2026-06-11 17:40:40 +0530

    final version

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./cms.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'cms.db')}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
